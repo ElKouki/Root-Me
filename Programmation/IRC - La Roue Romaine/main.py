02eeed8a800f415ff1bde32d26023918a30a179c
ElKouki <kyllianguillou@gmail.com>
2021-07-10 14:42:14 +0200

    Update main.py

--- Programmation/IRC - La Roue Romaine/main.py
+++ Programmation/IRC - La Roue Romaine/main.py
@@ -1,30 +1,30 @@
-  
-import socket, sys, time, base64
-import math,re
-import codecs
-server = "irc.root-me.org"
-port = 6667
-channel = "#root-me_challenge"
-botnick = "rajoul"
-serverbot = "candy"
- 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("connecting to:"+server)
-irc.connect((server, port))
-irc.send(b"USER kouki kouki kouki :This is a fun bot!\n")
-irc.send(b"NICK kouki\n")
-irc.send(b"JOIN #root-me_challenge\n")
+# -*- coding: utf-8 -*-
 
- 
-irc.send(b'PRIVMSG candy :!ep3\r\n')
-text = irc.recv(2040)
-message=codecs.decode(str(text.strip(b'\r')),'rot_13')
-print(message)
-irc.send(b'PRIVMSG candy :!ep3 -rep %s\r\n'%message)
-text = irc.recv(2040)
-print(text)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = 'irc.root-me.org' #irc server
+PORT = 6667 #port
+NICK = 'kouki'
+USERNAME = 'kouki'
+
+print('soc created |', s)
+remote_ip = socket.gethostbyname(HOST)
+print('ip of irc server is:', remote_ip)
 
 
+s.connect((HOST, PORT))
 
+print('connected to: ', HOST, PORT)
 
+nick_cr = ('NICK ' + NICK + '\r\n').encode()
+s.send(nick_cr)
+usernam_cr= ('USER kouki kouki kouki :rainbow pie \r\n').encode()
+s.send(usernam_cr)
+s.send('JOIN #mysupertest \r\n'.encode()) #chanel
 
+while 1:
+    data = s.recv(4096).decode('utf-8')
+    print(data)
+
+s.close()
