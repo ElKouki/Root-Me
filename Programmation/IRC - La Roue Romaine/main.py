fc943c21974a8955dd50db7385603b231da6ba8b
ElKouki <76102821+ElKouki@users.noreply.github.com>
2021-07-11 19:00:49 +0200

    Update main.py

--- Programmation/IRC - La Roue Romaine/main.py
+++ Programmation/IRC - La Roue Romaine/main.py
@@ -1,30 +1,36 @@
-# -*- coding: utf-8 -*-
-
 import socket
+import math
+from codecs import decode
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = 'irc.root-me.org' #irc server
-PORT = 6667 #port
-NICK = 'kouki'
-USERNAME = 'kouki'
+#On crée un socket et on se connecte au serveur
+host="irc.root-me.org" # host du chall
+port=6667 # port IRC
 
-print('soc created |', s)
-remote_ip = socket.gethostbyname(HOST)
-print('ip of irc server is:', remote_ip)
+bot = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Création du socket utilisé par le bot
+bot.connect((host,port)) # on se connecte au host:port
 
+#On s'identifie
+bot.send(b"NICK kouki\r\n")
+print(bot.recv(1024000))
+bot.send(b"USER kouki 0 * :Real Name\r\n")
+print(bot.recv(1024000))
 
-s.connect((HOST, PORT))
+#On rejoint le channel
+bot.send(b"JOIN #root-me_challenge\r\n")
+print(bot.recv(20140000))
 
-print('connected to: ', HOST, PORT)
-
-nick_cr = ('NICK ' + NICK + '\r\n').encode()
-s.send(nick_cr)
-usernam_cr= ('USER kouki kouki kouki :rainbow pie \r\n').encode()
-s.send(usernam_cr)
-s.send('JOIN #mysupertest \r\n'.encode()) #chanel
-
-while 1:
-    data = s.recv(4096).decode('utf-8')
-    print(data)
-
-s.close()
+#On envoie un 1er message
+message = bot.send(b"PRIVMSG Candy !ep3\r\n")
+txt = bot.recv(128)
+print(txt)
+txt = txt.split(b" :")[1].replace(b' ',b'')
+txt = str(txt)
+print(txt)
+rep = txt.split('\'')[1].split('\\')[0]
+print(rep)
+#On décode le message envoyé par le bot
+txt2 = decode(str(rep), 'rot_13')
+print(txt2)
+message = bot.send(b"PRIVMSG Candy !ep3 -rep " + bytes(txt2, encoding='utf-8') + b'\r\n')
+#On récupère le flag
+print(bot.recv(1024000))
